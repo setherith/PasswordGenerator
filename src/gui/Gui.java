d4995d9007da90868efc8124b9acbfb478a136cb
Shane Pudner <setherith@gmail.com>
2016-10-30 00:45:52 +0100

    Initial commit

---
+++ src/gui/Gui.java
@@ -0,0 +1,96 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+
+public class Gui extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8976021562072214389L;
+	private JPanel contentPane;
+	private JTextField txtPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui frame = new Gui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Gui() {
+		setTitle("Password Generator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 212, 104);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		txtPassword = new JTextField();
+		txtPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		txtPassword.setEditable(false);
+		GridBagConstraints gbc_txtPassword = new GridBagConstraints();
+		gbc_txtPassword.insets = new Insets(0, 0, 5, 0);
+		gbc_txtPassword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPassword.gridx = 0;
+		gbc_txtPassword.gridy = 0;
+		contentPane.add(txtPassword, gbc_txtPassword);
+		txtPassword.setColumns(10);
+		
+		JButton btnGenerate = new JButton("Generate");
+		btnGenerate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Generate password
+				String result = "";
+				Random r = new Random();
+				for (int x = 0; x < 6; x++) {
+					// all printable = 33 -> 126
+					// specials = 33 -> 47, 58 -> 64, 91 -> 96, 123 -> 126
+					// numbers = 48 -> 57
+					// upper case letter = 65 -> 90
+					// lower case letter = 97 -> 122
+					// ??? consider vowels only ???
+					result += (char) (r.nextInt(26) + 97);
+				}
+				txtPassword.setText(result);
+				
+			}
+		});
+		GridBagConstraints gbc_btnGenerate = new GridBagConstraints();
+		gbc_btnGenerate.gridx = 0;
+		gbc_btnGenerate.gridy = 1;
+		contentPane.add(btnGenerate, gbc_btnGenerate);
+	}
+
+}
